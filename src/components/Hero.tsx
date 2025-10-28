c5502582c28aa245a23d54e1601bcfe8da2938bf
Ibrahima Gueye <ibrahimagueye021@gmail.com>
2025-10-28 10:45:00 +0100

    Nouvelle PHOTOS

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,12 @@
-import { ImageWithFallback } from './figma/ImageWithFallback';
+//import { ImageWithFallback } from './figma/ImageWithFallback';
 
 export function Hero() {
   return (
     <section id="accueil" className="relative h-[600px] flex items-center">
       <div className="absolute inset-0">
         <ImageWithFallback
-          src="https://images.unsplash.com/photo-1759683730016-1c3c6e526cfb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxqZXdlbHJ5JTIwc3RvcmUlMjBsdXh1cnl8ZW58MXx8fHwxNzYxMjg5NzUzfDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral"
+        src="/images/hero-bijouterie.jpg.jpg"
+          //src="https://images.unsplash.com/photo-1759683730016-1c3c6e526cfb?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxqZXdlbHJ5JTIwc3RvcmUlMjBsdXh1cnl8ZW58MXx8fHwxNzYxMjg5NzUzfDA&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral"
           alt="Bijouterie Yacine"
           className="w-full h-full object-cover"
         />
