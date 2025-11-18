9f8e854219ddfd65c92fc8a951533ed64abbf6a4
Ibrahima Gueye <ibrahimagueye021@gmail.com>
2025-11-18 11:44:17 +0100

    button whatsapp

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ export function Header() {
               </a>
             </div>
             <div className="hidden md:block">
-              <span>Guédiawaye Marché SHAM </span>
+              <span>Guédiawaye Marché SHAME </span>
             </div>
           </div>
         </div>
