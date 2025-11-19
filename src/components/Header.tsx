57029c421968d987753fa0752130e7ce60660dbb
Ibrahima Gueye <ibrahimagueye021@gmail.com>
2025-11-19 20:22:37 +0100

    refactoring

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,7 +17,7 @@ export function Header() {
               </a>
             </div>
             <div className="hidden md:block">
-              <span>Guédiawaye Marché Shame </span>
+              <span>Guédiawaye Marché Sham </span>
             </div>
           </div>
         </div>
